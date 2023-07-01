384f946891bf8aee37cdd7cd45667e4f2c8e85ed
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-07-01 23:15:40 +0800

    trying out strings

--- pythonbelike.py
+++ pythonbelike.py
@@ -1,2 +1,9 @@
 print("test file lol")
 print(50)
+
+this_is_a_string = "text in the string lol. you can use either single or double quotes"
+
+def function1():
+    print("this print is from function1")
+
+function1()
