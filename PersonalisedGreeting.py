708f782b8eb5d1196058e32e43c99c064437a08b
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-07-19 18:18:34 +0800

    learning print and fstring

--- PersonalisedGreeting.py
+++ PersonalisedGreeting.py
@@ -5,11 +5,18 @@ welcometext = "\n\nHello!\n To generate a personalised greeting, we'll ask you s
 print(welcometext)
 
 name = input("First off, what's your name?: ")
-age  = int(input("What's your age this year?: ")) # let's do x years old born in xxxx
+age  = int(input("What's your age this year?: ")) # let's do: "x years old born in xxxx"
 birthyear = 2023-age
 country = input("Where are you from?: ")
 hobby = input("What is your favourite hobby?: ")
 
+print("\nHi", name, "from", country, sep=' ', end='\n')
+print("You're", age, "years old, born in", birthyear, end=".\n")
+print("You also love", hobby, end="!\n")
+
+
+''' old code lol
 print("\n\nHi " +str(name), "from", country,"!")
 print("You're " +str(age), "years old, born in", birthyear, ".")
 print("And you LOVE", hobby, "\n")
+'''
--- print_syntax_tests.py
+++ print_syntax_tests.py
@@ -1,5 +1,13 @@
-firstword = "your "
-secondword = "mom"
-number = 13
+name = "kendrick"
+age = 23
 
-print(firstword + secondword + " gay" , str(number))
+#print(f"hello, {name}! you're {age} this year\n")
+
+message = (
+    f"Hi, {name}! "
+    f"You're {age} this year!\n"
+)
+
+print(message)
+
+#print(f'This sentence has a \"quoted\" word inside it')
