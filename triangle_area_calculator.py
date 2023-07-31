e0f1558010a6715307fde6731b56eb16af8ae34d
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-07-31 12:29:15 +0800

    made triangle area calculator

---
+++ triangle_area_calculator.py
@@ -0,0 +1,8 @@
+#calculates area of triangle
+
+print("\nTriangle Area calculator using 1/2BH")
+base = int (input("Base: "))
+height = int (input("Height: "))
+
+area = 0.5 * base * height
+print("Area =", area)
