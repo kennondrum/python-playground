00c23d649c882be4de715dedbdd92046020af8bf
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-08-08 18:59:45 +0800

    playing with if statements

---
+++ elif testing.py
@@ -0,0 +1,14 @@
+age = 40
+
+if age >= 21:
+    print("adult")
+if age > 30:
+    print("old af")
+elif age < 21:
+    print("jail bait")
+else:
+    print("error lol")
+
+'''
+so elif basically groups if statements, and will exit the whole "group" once one of them is TRUE
+'''
