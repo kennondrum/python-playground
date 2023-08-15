e8eb194b59d0bd8a629b0b45bd745dca3b36f70e
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-08-15 18:16:33 +0800

    learning loops

---
+++ for loop.py
@@ -0,0 +1,7 @@
+for x in range(1, 10):
+    if x == 5:
+        break
+    else:
+        print("loop number:", x)
+
+print(x)
---
+++ while loop.py
@@ -0,0 +1,8 @@
+i = 1
+
+while i < 10:
+    print("loop number:", i)
+    i += 1
+    if i == 5:
+        break
+print("i =", i)
