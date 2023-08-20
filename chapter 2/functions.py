6fd8b15b88beff8855fa6d43ca126467e27a75b1
Kendrick Tan <kendricktanruihao@hotmail.com>
2023-08-20 09:30:38 +0800

    functions and return functions

--- chapter 2/functions.py
+++ chapter 2/functions.py
@@ -1,4 +1,4 @@
- #defines function with input parameters
+#define function with input parameters
 def happy_birthday(name, age):
     print("Happy Birthday", name)
     print("You are", age, "years old\n")
@@ -7,3 +7,20 @@ def happy_birthday(name, age):
 happy_birthday("dick", 21)
 happy_birthday("dumbass", 30)
 happy_birthday("cock", 23)
+
+#define function to add two arguments
+def add(x, y):
+    z = x + y
+    return z    #sends out the sum
+
+print(add(1,2)) #call the function and print out the answer
+
+#define function to create a full name
+def create_name(first, last):
+    first = first.capitalize() #capitalises both strings
+    last = last.capitalize()
+    return first + " " + last #return as a combined string
+
+#call the combined string "full_name"
+full_name = create_name("spongebob", "squarepants")
+print(full_name) #prints out the full_name
